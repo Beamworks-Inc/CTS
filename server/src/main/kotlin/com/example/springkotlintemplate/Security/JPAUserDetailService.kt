6749164dcc19b5c6638b6e329eafb05c21de59fc
BEOKS <lee01042000@gmail.com>
2023-03-02 16:07:46 +0900

    Fix : 임시 사용자 레포지토리 삭제

--- server/src/main/kotlin/com/example/springkotlintemplate/Security/JPAUserDetailService.kt
+++ server/src/main/kotlin/com/example/springkotlintemplate/Security/JPAUserDetailService.kt
@@ -17,17 +17,3 @@ class JPAUserDetailService(
         return org.springframework.security.core.userdetails.User(user.email, user.password, emptyList())
     }
 }
-
-
-@Component
-class UserRepositoryImpl(): UserRepository {
-    override fun findByUsername(username: String): User? {
-        val passwordEncoder= BCryptPasswordEncoder()
-        return when(username){
-            "researcher" -> User(1, "researcher", passwordEncoder.encode("researcher"), Role.RESEARCHER)
-            "reviewer" -> User(2, "reviewer", "reviewer", Role.REVIEWER)
-            "pi" -> User(3, "pi", "pi", Role.PI)
-            else -> null
-        }
-    }
-}
