8460266797663fcb20827a4a0c4847b32f57acdf
Henry-Hong <hhr8540@naver.com>
2023-03-06 08:40:39 +0900

    Feat : Create-Vite-App 불필요 파일 제거 & 라우터 세팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,19 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
+    <BrowserRouter>
+      {/* <Box sx={{ display: 'flex' }}> */}
+      {/* <CssBaseline />
+        <AppBar />
+        <Drawer /> */}
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는 경우 처리 */}
+      </Routes>
+      {/* </Box> */}
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/LoginPage/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoginPage = () => {
+  const onLoginButtonClick = () => {};
+
+  return <div>login page</div>;
+};
+
+export default LoginPage;
